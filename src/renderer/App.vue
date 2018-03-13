235a990d92b06d1966f4d75702f68acfd62a5196
Lucy20209060 <550502661@qq.com>
2018-03-13 22:13:48 +0800

    base code

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-test-01'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
